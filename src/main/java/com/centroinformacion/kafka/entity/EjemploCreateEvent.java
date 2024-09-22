5113d6eb7afc8b2acc41d4bd7315011ce92841ff
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-09-22 17:22:04 -0500

    aaaa

--- src/main/java/com/centroinformacion/kafka/entity/EjemploCreateEvent.java
+++
@@ -1,9 +0,0 @@
-package com.centroinformacion.kafka.entity;
-
-import com.centroinformacion.entity.Ejemplo;
-import com.centroinformacion.kafka.config.Event;
-
-public class EjemploCreateEvent extends Event<Ejemplo>{
-
-	
-}
